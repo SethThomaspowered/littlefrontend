6e2e6c3d7b5fd5eeaef7a8460f0ae1e9c0fead62
Seth Thomas <athomas.seth@gmail.com>
2022-02-01 13:00:32 -0500

    updated environment files

--- src/app/bookdetail/bookdetail.component.ts
+++ src/app/bookdetail/bookdetail.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { BackendService } from '../backend.service';
 import { BOOKS } from '../library/books';
+import { Book } from '../book.model';
 
 @Component({
   selector: 'app-bookdetail',
@@ -9,24 +10,29 @@ import { BOOKS } from '../library/books';
   styleUrls: ['./bookdetail.component.css']
 })
 export class BookdetailComponent  implements OnInit {
-
+  id: number|undefined;
   gBBook: any;
   gBBookBacks: any;
   books= BOOKS;
+  book: any;
+  
   constructor(private route: ActivatedRoute, private backend: BackendService) { }
   findGBBooks(){
     this.backend.getGBBooks().subscribe(response => {
       console.log(response);
-      return this.gBBookBacks= response});
+      this.route.paramMap.subscribe(params => {
+        this.book = BOOKS.find(post => {
+          let paramId: string = params.get("id") || "";
+          return post.id === parseInt(paramId);
+        });
+      });
+      this.gBBookBacks= response});
   }
   ngOnInit(): void {
     this.findGBBooks();
-    this.route.paramMap.subscribe(params => {
-      this.gBBook = BOOKS.find(book =>{
-        let paramId: string = params.get("id") || "";
-        return  book.id === parseInt(paramId);
-      });
-    });
+    
+    
+    
   }
 
 }
