6cc94c3e6337bac1b885810cbc4c145e68c639f4
Seth Thomas <athomas.seth@gmail.com>
2022-02-01 11:00:00 -0500

    made changes to routing

--- src/app/bookdetail/bookdetail.component.ts
+++ src/app/bookdetail/bookdetail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BackendService } from '../backend.service';
+import { BOOKS } from '../library/books';
 
 @Component({
   selector: 'app-bookdetail',
@@ -11,6 +12,7 @@ export class BookdetailComponent  implements OnInit {
 
   gBBook: any;
   gBBookBacks: any;
+  books= BOOKS;
   constructor(private route: ActivatedRoute, private backend: BackendService) { }
   findGBBooks(){
     this.backend.getGBBooks().subscribe(response => {
@@ -20,10 +22,10 @@ export class BookdetailComponent  implements OnInit {
   ngOnInit(): void {
     this.findGBBooks();
     this.route.paramMap.subscribe(params => {
-      
+      this.gBBook = BOOKS.find(book =>{
         let paramId: string = params.get("id") || "";
-        return this.gBBook.id === parseInt(paramId);
-     
+        return  book.id === parseInt(paramId);
+      });
     });
   }
 
