706a6be2334aef1fa6ef320210bd7d9233854cc1
Seth Thomas <athomas.seth@gmail.com>
2022-02-01 10:24:48 -0500

    made changes to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookdetailComponent } from './bookdetail/bookdetail.component';
 import { HomeComponent } from './home/home.component';
 import { LibraryComponent } from './library/library.component';
 import { NotfoundComponent } from './notfound/notfound.component';
@@ -8,7 +9,8 @@ import { SearchGBComponent } from './search-gb/search-gb.component';
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: "library", component: LibraryComponent},
-  { path: "library/1/books/:id", component: SearchGBComponent},
+  { path: "library/1/books/:id", component: BookdetailComponent},
+  { path: "hippo", component: SearchGBComponent},
   { path: "**", pathMatch: 'full', component: NotfoundComponent}
 ];
 
