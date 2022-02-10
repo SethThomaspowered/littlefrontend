854db7036ed134aadced276f0e61253cf80e4d9e
Seth Thomas <athomas.seth@gmail.com>
2022-02-10 17:53:21 -0500

    Update routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  currentId: any;
+  id: any;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.currentId= this.route.snapshot.paramMap.get('id');
+    this.id = parseInt(this.currentId);
   }
 
 }
